02b159cf71cab2330ad7a5c8cb0dad42f16d000e
Zhifeng Wang <zw4360@gadi-login-05.gadi.nci.org.au>
2025-04-29 23:42:17 +1000

    delete other sh

--- run_multiple.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-module load openmpi
-
-echo "PBS_NODEFILE points to: $PBS_NODEFILE"
-cat $PBS_NODEFILE  # See which nodes you're using
-
-mpirun -np 2 --map-by ppr:1:node --hostfile "$PBS_NODEFILE" \
-    sh official_provided_train_scription_on_github.sh
